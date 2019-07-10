9071c0c81ad98113fa46547d98d324871c679269
Thomas McClellan <thomasmcclellan87@gmail.com>
2019-07-09 21:29:54 -0400

    added fake data to allPlants.json and wip add specific plant from allPlants to myPlants

--- client/src/app/modal/modal.component.ts
+++ client/src/app/modal/modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import myPlants from '../../myPlants.json';
 
 @Component({
   selector: 'app-modal',
@@ -9,21 +10,47 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class ModalComponent implements OnInit {
   _plantData: object = {}
-  _plots: any = [{ name: 'Plot 1' }, { name: 'Plot 2' }, { name: 'Plot 3' }]
+  _plots: Array<any> = myPlants
+  plantCrop: FormGroup
   
-  public form: FormGroup = new FormGroup({
-    quantity: new FormControl('')
-  })
+  // public form: FormGroup = new FormGroup({
+  //   quantity: new FormControl('')
+  // })
 
-  constructor(@Inject(MAT_DIALOG_DATA) public plantData: any) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public plantData: any, private fb: FormBuilder) { }
 
   ngOnInit() {
     this._plantData = this.plantData
-    console.log(this._plots)
-
-    this.form.setValue({
-      quantity: this._plantData['plantsPerSquareFoot']
+    console.log(this._plantData)
+    
+    this.plantCrop = this.fb.group({
+      // quantity: new FormControl().setValue({
+      //   quantity: this._plantData['plantsPerSquareFoot']
+      // }),
+      quantity: new FormControl(),
+      plot: new FormControl()
     })
+
+    // this.form.setValue({
+    // })
+
   }
 
+  onPlant(): void {
+    const newPlant: object = {}
+
+    newPlant['plantName'] = this._plantData['plantName']
+    newPlant['plantsPerSquareFoot'] = this._plantData['plantsPerSquareFoot']
+    newPlant['quantity'] = this.plantCrop.value['quantity']
+
+    this._plots.map(plot => (plot.name === this.plantCrop.value['plot']) ? plot.plants.unshift(newPlant) : false)
+  }
 }
+
+// TODO: Have quanity and space filled adjust onSubmit of modal
+
+// TODO: Create way to check sow dates and then a conditional that says too early or too late to plant based on if outside that range
+
+// TODO: Create ability to create new plot based on "create new plot" link
+
+// TODO: Format modal to look nicer
